51a8e205b506696c239b4fca67c5469d2dfae02b
eunjin <eunjin.kang22@gmail.com>
2023-04-27 17:17:34 +0900

    MVC update

--- controler.py
+++ controler.py
@@ -1,36 +1,59 @@
 import sys
 from PySide2.QtWidgets import QWidget, QApplication
 
-from ui import UploadUI
-from model import Test
-
-from shotgun_api3 import Shotgun
-from pprint import pprint
-
-SERVER_PATH = "https://rndtest.shotgrid.autodesk.com"
-SCRIPT_NAME = 'script_kej'
-SCRIPT_KEY = 'kibjce#prQeimq3lsojkstgmq'
-
-sg = Shotgun(SERVER_PATH, SCRIPT_NAME, SCRIPT_KEY)
+from ui import AssetUploaderView
+from model import AssetUploaderModel
 
 
-class ControlTest(QWidget):
+class AssetUploaderControler(QWidget):
     def __init__(self):
         super().__init__()
-        self.model = Test()
-        self.view = UploadUI()
+        self.model = AssetUploaderModel()
+        self.view = AssetUploaderView()
 
         # 따로 test
-        self.view.com_project.addItem(self.set_project()) 
+        self.view.com_project.addItems(self.model.get_project_name())
         self.view.com_asset.addItems(self.model.get_asset_type())
 
         self.view.com_project.activated.connect(self.current_project)
         self.view.com_asset.activated.connect(self.current_asset_type)
 
-        self.view.btn_ok.clicked.connect(self.upload_assets)
-        # self.view.btn_browse.clicked.connect(self.model.get_dir_path)
-        self.view.btn_cancel.clicked.connect(self.btn_cancel_clicked)
 
+        self.view.btn_browse.clicked.connect(self.btn_browse_clicked)
+        self.view.btn_upload.clicked.connect(self.btn_upload_clicked)
+        self.view.btn_cancel.clicked.connect(self.btn_cancel_clicked)
+    
+
+
+    def current_project(self):
+        project = self.view.com_project.currentText()
+        return project
+
+    def current_asset_type(self):
+        asset_type = self.view.com_asset.currentText()
+        return asset_type
+    
+    def btn_browse_clicked(self):
+        self.close()
+
+    def btn_upload_clicked(self):
+        self.close()
+        
+    def btn_cancel_clicked(self):
+        self.close()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    AssetUploaderControler()
+
+    sys.exit(app.exec_())
+
+
+
+
+
+##test
 
         # 연동 test
         # aa = self.model.get_project()
@@ -49,34 +72,11 @@ class ControlTest(QWidget):
     #     if type:
     #         self.view.com_asset.addItems(type)
 
-    def set_project(self):
+    # def set_project(self):
+        # temps = self.model.get_project() 
         # temp = []
-        temp = self.model.get_project()
-        print(temp)
+        # for t in temps[1]:
+        #     temp.append(t)
+
+        # print(temp)
         # return temp["name"]
-
-    def current_project(self):
-        project = self.view.com_project.currentText()
-        return project
-
-    def current_asset_type(self):
-        type = self.view.com_asset.currentText()
-        return type
-
-    def upload_assets(self):
-        data = {
-            'project': {"type":"Project", "name": self.current_project()},
-            'code': 'dino 003', # file name
-            'sg_asset_type': self.current_asset_type() #type_name
-            }
-        result = sg.create('Asset', data)
-        print(result)
-    
-    def btn_cancel_clicked(self):
-        self.close()
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    ct = ControlTest()
-
-    sys.exit(app.exec_())
--- ui.py
+++ ui.py
@@ -1,10 +1,9 @@
 import sys
 
-from PySide2.QtWidgets import QWidget, QComboBox, QHBoxLayout, QVBoxLayout, QLineEdit, QApplication, QPushButton, QFileDialog
-from PySide2.QtGui import *
+from PySide2.QtWidgets import QWidget, QComboBox, QHBoxLayout, QVBoxLayout, QLineEdit, QApplication, QPushButton
 
 
-class UploadUI(QWidget):
+class AssetUploaderView(QWidget):
     def __init__(self):
         super().__init__()
         self.setup_ui()
@@ -13,13 +12,15 @@ class UploadUI(QWidget):
         self.setWindowTitle("Upload Assets")
         self.setGeometry(200, 200, 600, 200)
 
+        # create widgets
         self.com_project = QComboBox()
         self.com_asset = QComboBox()
         self.line_path = QLineEdit()
         self.btn_browse = QPushButton("Browse")
-        self.btn_ok = QPushButton("Ok")        
+        self.btn_upload = QPushButton("Upload")        
         self.btn_cancel = QPushButton("cancel")        
 
+        # layout
         self.vb = QVBoxLayout()
         self.hbTop = QHBoxLayout()
         self.hbMid = QHBoxLayout()
@@ -36,22 +37,29 @@ class UploadUI(QWidget):
         self.hbMid.addWidget(self.btn_browse)
 
         self.hbBot.addStretch()
-        self.hbBot.addWidget(self.btn_ok)
+        self.hbBot.addWidget(self.btn_upload)
         self.hbBot.addWidget(self.btn_cancel)
         self.hbBot.addStretch()
 
-        self.btn_browse.clicked.connect(self.get_asset_name)
+        # button click event example
+        self.btn_browse.clicked.connect(self.btn_browse_clicked)
+        self.btn_upload.clicked.connect(self.btn_upload_clicked)
+        self.btn_cancel.clicked.connect(self.btn_cancel_clicked)
 
         self.show()
 
-    def get_asset_name(self):
-        test = QFileDialog.getOpenFileName()
-        print(test)
+    def btn_browse_clicked(self):
+        print("Select Assets")
+
+    def btn_upload_clicked(self):
+        print("Upload Assets")
+
+    def btn_cancel_clicked(self):
+        print("close")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    uu = UploadUI()
-    
+    ui= AssetUploaderView()
     sys.exit(app.exec_())
     
